3326ec8ca8863d56dbce73a0f7bdda3f95c0c144
Your Name <your_email@whatever.com>
2025-01-19 07:33:30 +0500

    initial commit

---
+++ LAB1/Variable/ex.py
@@ -0,0 +1,27 @@
+#ex1
+carname = "Volvo"
+
+#ex2
+x=50
+
+#ex3
+x=4
+y=5
+print(x+y)
+
+#ex4
+x=5
+y=6
+z=x+y
+ptint(z)
+
+#ex5
+x,y,z = "Orange", "Banana", "Cherry"
+
+#ex6
+x =y=z ="Orange"
+
+#ex7
+def myfunc():
+  global x
+  x = "fantastic"
